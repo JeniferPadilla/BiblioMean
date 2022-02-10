bccc7ca0bb5ce2735ee7ad26b180bc14ef071a42
JeniferPadilla <karin_padilla21@hotmail.com>
2022-02-10 16:27:25 -0500

    adelanto de backend de BiblioMEAN

---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const dbConnection = () =>{
+    try {
+        mongoose.connect(process.env.DB_CONNECTION,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });
+        console.log("Connection whith MongoDB: OK");
+    } catch (e) {
+        console.log("Error connection to MongoDB: \n", e);
+    }
+};
+export default {dbConnection};

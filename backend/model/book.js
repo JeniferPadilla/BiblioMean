3346bc5bd5846b56913abb701cc6c8de706281e6
JeniferPadilla <karin_padilla21@hotmail.com>
2022-02-11 10:27:36 -0600

    se crearon las colecciones de model

---
+++ backend/model/book.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const bookSchema= new mongoose.Schema({
+
+    title:String,
+    author:String,
+    editorial:String,
+    category: String,
+    description:String,
+    quantity:Number,
+    deliveryDate:Date,
+    rol:{type:mongoose.Schema.ObjectId,ref:"roles"},
+    registerDate:{type:Date, default:Date.now},
+    dbStatus:true,
+
+});
+
+const book= mongoose.model("books", bookSchema);
+export default book;
